169a31a46e11e97409695062fe5031d6bc2852f2
pev9a <vopovadk1@gmail.com>
2025-01-17 17:00:20 +0500

    added afterEach

--- src/test/java/ru/scooter/helpers/TestBase.java
+++ src/test/java/ru/scooter/helpers/TestBase.java
@@ -1,17 +1,25 @@
 package ru.scooter.helpers;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void beforeEach() {
 
         Configuration.baseUrl = "https://qa-scooter.praktikum-services.ru/";
         Configuration.browser = "firefox";
         Configuration.browserSize = "1920x1080";
+
+    }
+
+    @AfterEach
+    void afterEach(){
+
+        Selenide.closeWebDriver();
 
     }
 
